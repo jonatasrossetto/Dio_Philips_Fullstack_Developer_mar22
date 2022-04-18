04fe451664ae2f5ff3e71c35400b2a4af9abfa4d
Jonatas Rossetto <jonatas.rossetto@gmail.com>
2022-04-18 16:15:58 -0300

    update 18-04-2022

--- Sintaxe e Operadores JS/script.js
+++ Sintaxe e Operadores JS/script.js
@@ -16,10 +16,10 @@ let n2 = promptNumber('Enter number n2');
 // Crie uma função que recebe dois números como parâmetro
 // Confira se os números são iguais
 // Confira se a soma dos números é maior que 10 ou menor que 20
-// Retorne uma string dizendo "Os numeros n1 e n2 não/são iguais. Sua soma é soma, que é maior/menor que 20"
+// Retorne uma string dizendo "Os numeros n1 e n2 não/são iguais. Sua soma é soma, que é maior/menor que 10 e maior/menor que 20"
 
 
-console.log(`Os números ${n1} e ${n2} ${n1==n2?'são':'não são'} iguais. Sua soma é igual à ${n1+n2} que é ${(n1+n2)<20?'menor':'maior'} que 20`);
+alert(`Os números ${n1} e ${n2} ${n1==n2?'são':'não são'} iguais. Sua soma é igual à ${n1+n2} que é ${(n1+n2)<10?'menor':'maior'} que 10 e ${(n1+n2)<20?'menor':'maior'} que 20`);
 
 // function comparaN1eN2(nA, nB){
 //     console.log(`Os números ${nA} e ${nB} ${na===nb?'são':'não são'} iguais`);
