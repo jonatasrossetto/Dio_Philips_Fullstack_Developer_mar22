651dc4181036477f9a6a454ac23a16b72cee267b
Jonatas Rossetto <jonatas.rossetto@gmail.com>
2022-05-12 14:18:15 -0300

    update 12-05-2022 with a git branch to test the use o modules in Angular

---
+++ Introducao Angular/course-manager/src/app/courses/course.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { CourseInfoComponent } from "./course-info.component";
+import { CourseListComponent } from "./course-list.component";
+
+@NgModule ({
+    declarations: [
+        CourseListComponent,
+        CourseInfoComponent
+    ],
+    imports: [
+        
+    ]
+})
+
+export class CourseModule {
+
+}
